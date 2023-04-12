734670d3c983700969c3ddb7b09deff109e516ed
suhaila <suhaila.c60@gmail.com>
2023-04-13 00:22:56 +0530

    updated filteredProd function

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,17 +2,13 @@ import ProductCard from "./ProductCard";
 
 function ProductList(props) {
   const filteredProd = props.Products.filter((products) => {
-    if (products.description !== undefined && products.category !== undefined) {
+    if (products.description !== undefined || products.category !== undefined) {
       if (
         products.description
           .toLowerCase()
-          .indexOf(props.newSearchText.toLowerCase()) > -1
+          .indexOf(props.newSearchText.toLowerCase() || props.newSuggest.toLowerCase() ) > -1
       )
-        if (
-          products.description
-            .toLowerCase()
-            .indexOf(props.newSuggest.toLowerCase()) > -1
-        )
+       
           if (products.category.indexOf(props.newBtncategory) > -1) {
             return true;
           }
