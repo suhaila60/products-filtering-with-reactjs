476b97dfdad0dcd9e986d23096d7f5e8c33bf2f3
suhaila <suhaila.c60@gmail.com>
2023-04-13 23:34:12 +0530

    re-created category buttons.

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,36 +1,25 @@
-import Button from "react-bootstrap/Button";
 
 export default function CategoryFilter(props) {
   const categoryButtonStyle = {
     color: "black",
     textDecoration: "none",
+    width: "80px",
+    
+    
   };
 
   const onClickCategoryBtn = (event) => {
     props.onButtonCategory(event.target.value);
   };
   return (
-    <div>
+    <div className="d-flex flex-column">
       <h6 className="fw-bold ">Category</h6>
-      <Button
-        className="categoryButton"
-        style={categoryButtonStyle}
-        variant="link"
-        value={"women"}
-        onClick={onClickCategoryBtn}
-      >
-        women
-      </Button>
-      <br />
-      <Button
-        className="categoryButton"
-        style={categoryButtonStyle}
-        variant="link"
-        value={"male"}
-        onClick={onClickCategoryBtn}
-      >
-        men
-      </Button>
+      <input type="radio" className="btn-check   " name="btnradio" id="btnradio1" autocomplete="off"  value={"women"}  onClick={onClickCategoryBtn} />
+  <label class="btn btn-outline-success btn-link mb-1" for="btnradio1" style={categoryButtonStyle}> women</label>
+  <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off"   value={"male"}   onClick={onClickCategoryBtn}/>
+  <label class="btn btn-outline-success btn-link" for="btnradio2"  style={categoryButtonStyle} >  men</label>
+
+     
     </div>
   );
 }
